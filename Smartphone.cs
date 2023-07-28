3f8088d2504f7b9f9cbe97b80dc0f7e381eb912f
Nikiths1 <nikith.s1@mphasis.com>
2023-07-28 15:47:49 +0530

    first commit

---
+++ Smartphone.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElectronicDeviceswithIntefaces
+{
+    public class Smartphone : IConnectable, IRechargeable, IDisplayable
+    {
+        bool con;
+        public Smartphone(bool con)
+        {
+            this.con = con;
+        }
+        public int Charge(int minutes)
+        {
+            return minutes;
+        }
+
+        public bool Connect()
+        {
+            if (con==true)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string Display()
+        {
+            string info = "Display Resolution :480*800\nRefresh Rate :60Hz\nColor Format :RGB\nScreen Size :5.5\nPPI :515(***HDPI)";
+            return info;
+        }
+    }
+}
